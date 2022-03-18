06ad0e11241457bd166faee531d9bcec9e53d665
Sebastian Kulinski <sebastian.kulinski@codurance.com>
2022-03-18 15:40:24 +0100

    add hooks automation

---
+++ hook-scripts/install-hooks.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+GIT_DIR=$(git rev-parse --git-dir)
+
+echo "Installing hooks..."
+# this command creates symlink to our pre-push script
+ln -s ../../hook-scripts/pre-push.sh $GIT_DIR/hooks/pre-push
+echo "Done"!
